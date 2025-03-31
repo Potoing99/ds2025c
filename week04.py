615ccebd666b57575bb411fbe4f6ba7898537903
Potoing99 <kdkysj0719@daum.net>
2025-03-31 15:26:58 +0900

    v1.7 linked list

--- week04.py
+++ week04.py
@@ -1,4 +1,3 @@
-import random
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -20,6 +19,19 @@ class LinkedList:
         current.link = Node(data)
 
 
+    def remove(self,target):
+        if self.head.data == target:
+            self.head = self.head.link
+            return #10,8 을 지울때 -9만 남을때 이용되는 코드임
+        current = self.head
+        previous = None
+        while current:
+            if current.data == target:
+                previous.link = current.link #중요한 코드임
+            previous = current
+            current = current.link
+
+
     def search(self, target):
         current = self.head
         while current.link:
@@ -39,17 +51,16 @@ class LinkedList:
         return out_texts + "END"
 
 
-# ll = LinkedList()
-# ll.append(8)
-# ll.append(10)
-# ll.append(-9)
-# print(ll)
-# print(ll.search(99))
-# print(ll.search(10))
-
-
 ll = LinkedList()
-for _ in range(10):
-    ll.append(random.randint(1,30))
+ll.append(8)
+ll.append(10)
+ll.append(-9)
 print(ll)
-print(ll.search(7))
+print(ll.search(99))
+print(ll.search(10))
+ll.remove(10)
+ll.remove(8)
+print(ll)
+
+
+
