51daeb867d1af859d9c87459fb78e518403ec4da
Potoing99 <kdkysj0719@daum.net>
2025-04-14 14:34:36 +0900

    v2.7 queue

--- week06.py
+++ week06.py
@@ -28,6 +28,7 @@ class Queue:
         self.front = self.front.link
         if self.front is None:
             self.rear = None
+        temp.link = None
         return temp.data
 
 
@@ -36,6 +37,6 @@ q.enqueue("Database")
 q.enqueue("Data structure")
 print(q.size, q.front.data, q.rear.data)
 print(q.dequeue())
-print(q.size, q.front.data, q.rear.data)
+#print(q.size, q.front.data, q.rear.data)
 print(q.dequeue())
 print(q.size, q.front, q.rear)
